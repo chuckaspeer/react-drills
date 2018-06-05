953adba8fffe3052ce2a913285e3020b5f05a518
chuckaspeer <chuckaspeer@gmail.com>
2018-06-04 19:18:42 -0500

    React-drills

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor (){
+  super();
+
+  this.state = {
+  
+  
+}
+}
+
   render() {
+    let foods=["spaghetti ", "ice cream ", "sushi ", "bologna ", "cheese "];
+         let myFoods = foods.map(food => <li>{food}</li>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <ol>
+      {myFoods}
+      </ol>
+      
       </div>
     );
   }
